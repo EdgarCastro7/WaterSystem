07add06875f644465b74b5b0f7dd87a772781e96
EdgarCastro <cafucastro12@gmail.com>
2026-09-21 15:59:16 +0100

    Initialize WaterSystem project

---
+++ WaterSystem/WaterSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WaterSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
